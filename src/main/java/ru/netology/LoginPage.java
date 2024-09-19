709cff3b9009dd389f431f329b9208aec286c714
MikhailD <proxdme@mail.ru>
2024-09-19 12:07:04 +0300

    2 bugs

--- src/main/java/ru/netology/LoginPage.java
+++ src/main/java/ru/netology/LoginPage.java
@@ -1,36 +1,46 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class LoginPage {
-    private final SelenideElement byeField = $("#root > div > button:nth-child(3)");
-    private final SelenideElement cardNumberField = $("#root > div > form > fieldset > div:nth-child(1) > span > span > span.input__box > input");
-    private final SelenideElement monthField = $("#root > div > form > fieldset > div:nth-child(2) > span > span:nth-child(1) > span > span > span.input__box > input");
-    private final SelenideElement yearField = $("#root > div > form > fieldset > div:nth-child(2) > span > span:nth-child(2) > span > span > span.input__box > input");
-    private final SelenideElement cardHolderField = $("#root > div > form > fieldset > div:nth-child(3) > span > span:nth-child(1) > span > span > span.input__box > input");
-    private final SelenideElement cvcField = $("#root > div > form > fieldset > div:nth-child(3) > span > span:nth-child(2) > span > span > span.input__box > input");
-    private final SelenideElement submitButton = $("#root > div > form > fieldset > div:nth-child(4) > button > span > span");
-    private final SelenideElement successNotification = $("#root > div > div.notification.notification_visible.notification_status_ok.notification_has-closer.notification_stick-to_right.notification_theme_alfa-on-white > div.notification__title");
-    private final SelenideElement errorNotification = $("[data-test-id='error-notification'] .notification__content");
+    //private final SelenideElement byeField = $$(".tab-item").find(exactText("Купить"));
+    private final SelenideElement cardNumberField = $(" #root form fieldset div span input");
+    private final SelenideElement monthField = $("#root form fieldset div:nth-child(2) span span:nth-child(1) .input__box input");
+    private final SelenideElement yearField = $("#root form fieldset div:nth-child(2) span span:nth-child(2) .input__box input");
+    private final SelenideElement cardHolderField = $("#root form fieldset div:nth-child(3) span span:nth-child(1) .input__box input");
+    private final SelenideElement cvcField = $(" #root form fieldset div:nth-child(3) span span:nth-child(2) .input__box input");
+
+    private final SelenideElement submitButton = $$("button").find(exactText("Продолжить"));
+    //private final SelenideElement successNotification = $("");
+    //private final SelenideElement errorNotification = $("[data-test-id='error-notification'] .notification__content");
 
 
-    public void verifySuccessNotification(String cardNumber, String month, String year, String cardHolder, String cvc, String expectedText) {
-        byeField.click();
+
+
+    public void fillFormAndSubmit(String cardNumber, String month, String year, String cardHolder, String cvc) {
         cardNumberField.setValue(cardNumber);
         monthField.setValue(month);
         yearField.setValue(year);
         cardHolderField.setValue(cardHolder);
         cvcField.setValue(cvc);
+
         submitButton.click();
-        successNotification.shouldHave(exactText(expectedText)).shouldBe(visible);
     }
 
-    // Метод для проверки ошибки при отправке формы
-    public void verifyErrorNotification(String expectedText) {
-        errorNotification.shouldHave(exactText(expectedText)).shouldBe(visible);
+    // Метод для проверки успешного уведомления
+    public void verifySuccessNotification() {
+        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    // Метод для проверки сообщения об ошибке
+    public void verifyErrorNotification(String expectedErrorMessage) {
+        $(withText(expectedErrorMessage)).shouldBe(visible);
     }
 }
+
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -9,6 +9,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$$;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -23,6 +25,7 @@ public class FirstTest {
     void setUp() {
         // Открываем браузер с нужным URL через Selenide
         Selenide.open("http://localhost:8080/");
+        $$(".button__content").find(exactText("Купить")).click();
         loginPage = new LoginPage(); // Инициализируем страницу с формой
 
         // Настройка базы данных
@@ -34,14 +37,14 @@ public class FirstTest {
     @Test
     public void testSuccessfulDebitCardPayment() {
         // Заполняем форму через Selenide и проверяем успешное сообщение
-        loginPage.verifySuccessNotification(
+        loginPage.fillFormAndSubmit(
                 "1111 2222 3333 4444",   // Номер карты
                 "05",                    // Месяц
                 "25",                    // Год
                 "Ivan Ivanov",            // Владелец карты
-                "123",                    // CVC
-                "Успешно"                 // Ожидаемый текст успешного уведомления
+                "123"                     // CVC
         );
+        loginPage.verifySuccessNotification(); // Проверка успешного уведомления
 
         // Проверка записи в базе данных
         try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
@@ -57,5 +60,153 @@ public class FirstTest {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void testDeclinedDebitCardPayment() {
+        loginPage.fillFormAndSubmit(
+                "5555 6666 7777 8888",   // Номер отклонённой карты
+                "05",                    // Месяц
+                "25",                    // Год
+                "Ivan Ivanov",            // Владелец карты
+                "123"                     // CVC
+        );
+        loginPage.verifyErrorNotification("Ошибка"); // Проверка сообщения об ошибке
+
+        // Проверка записи в базе данных
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM payments WHERE card_number='4442 4444 4444 4444'");
+
+            // Убедимся, что запись существует
+            assertTrue(resultSet.next());
+            assertEquals("DECLINED", resultSet.getString("status"));
+
+            resultSet.close();
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testInvalidDebitCardNumber() {
+        loginPage.fillFormAndSubmit(
+                "4444 4444 4444",       // Номер карты короче 16 символов
+                "05",                  // Месяц
+                "25",                  // Год
+                "Ivan Ivanov",         // Владелец карты
+                "123"                  // CVC
+        );
+        loginPage.verifyErrorNotification("Неверный формат");
+    }
+
+    @Test
+    public void testEmptyCardNumber() {
+        loginPage.fillFormAndSubmit(
+                "",                     // Пустое поле номера карты
+                "05",                  // Месяц
+                "25",                  // Год
+                "Ivan Ivanov",         // Владелец карты
+                "123"                  // CVC
+        );
+        loginPage.verifyErrorNotification("Неверный формат");
+    }
+
+    @Test
+    public void testInvalidMonth() {
+        loginPage.fillFormAndSubmit(
+                "4444 4444 4444 4444", // Номер карты
+                "13",                  // Неверный месяц (больше 12)
+                "25",                  // Год
+                "Ivan Ivanov",         // Владелец карты
+                "123"                  // CVC
+        );
+        loginPage.verifyErrorNotification("Неверно указан срок действия карты");
+    }
+
+    @Test
+    public void testEmptyMonth() {
+        loginPage.fillFormAndSubmit(
+                "4444 4444 4444 4444", // Номер карты
+                "",                    // Пустое поле месяца
+                "25",                  // Год
+                "Ivan Ivanov",         // Владелец карты
+                "123"                  // CVC
+        );
+        loginPage.verifyErrorNotification("Неверный формат");
+    }
+
+    @Test
+    public void testInvalidYear() {
+        loginPage.fillFormAndSubmit(
+                "4444 4444 4444 4444", // Номер карты
+                "05",                  // Месяц
+                "23",                  // Неверный год (меньше текущего)
+                "Ivan Ivanov",         // Владелец карты
+                "123"                  // CVC
+        );
+        loginPage.verifyErrorNotification("Истёк срок действия карты");
+    }
+
+    @Test
+    public void testEmptyYear() {
+        loginPage.fillFormAndSubmit(
+                "4444 4444 4444 4444", // Номер карты
+                "05",                  // Месяц
+                "",                    // Пустое поле года
+                "Ivan Ivanov",         // Владелец карты
+                "123"                  // CVC
+        );
+        loginPage.verifyErrorNotification("Неверный формат");
+    }
+
+    @Test
+    public void testInvalidCardHolder() {
+        loginPage.fillFormAndSubmit(
+                "4444 4444 4444 4444", // Номер карты
+                "05",                  // Месяц
+                "25",                  // Год
+                "Иван Иванов",         // Владелец карты (на русском)
+                "123"                  // CVC
+        );
+        loginPage.verifyErrorNotification("Неверный формат");
+    }
+
+    @Test
+    public void testEmptyCardHolder() {
+        loginPage.fillFormAndSubmit(
+                "4444 4444 4444 4444", // Номер карты
+                "05",                  // Месяц
+                "25",                  // Год
+                "",                    // Пустое поле владельца
+                "123"                  // CVC
+        );
+        loginPage.verifyErrorNotification("Поле обязательно для заполнения");
+    }
+
+    @Test
+    public void testInvalidCVC() {
+        loginPage.fillFormAndSubmit(
+                "4444 4444 4444 4444", // Номер карты
+                "05",                  // Месяц
+                "25",                  // Год
+                "Ivan Ivanov",         // Владелец карты
+                ""                     // Пустое поле CVC
+        );
+        loginPage.verifyErrorNotification("Неверный формат");
+    }
+
+    @Test
+    public void testIncompleteCVC() {
+        loginPage.fillFormAndSubmit(
+                "4444 4444 4444 4444", // Номер карты
+                "05",                  // Месяц
+                "24",                  // Год
+                "Ivan Ivanov",         // Владелец карты
+                "12"                   // Неполный CVC
+        );
+        loginPage.verifyErrorNotification("Неверный формат");
+
     }
 }
